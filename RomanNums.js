b3112154f8e044d55f86d5813247859e1d45fae9
jgr68 <john.ryan@rutgers.edu>
2014-03-04 05:17:38 -0500

    sanitized input a bit

--- RomanNums.js
+++ RomanNums.js
@@ -17,25 +17,44 @@ function romanToDigit(numeral) {
 
     var stack = new Array();
     var digit = 0;
+    
+    var vDetected = false;
+    var xDetected = false;
+    var iCount = 0;
+    var vCount = 0;
+    var xCount = 0;
+
     for(var i = 0; i < numeral.length; ++i)
         stack.push(numeral.charAt(i));
 
     while(stack.length > 0) {
         var tok = stack.pop().toLowerCase();
-        if (tok == 'i')
+        if (tok == 'i' && vDetected == false) {
             digit += 1;
+            iCount += 1;
+        }
         else {
             if (stack.length > 0 && peek(stack).toLowerCase() == 'i') {
                 digit -= 1;
                 stack.pop();
             }
-            if (tok == 'x')
+            if (tok == 'x') {
                 digit += 10;
-            else if (tok == 'v')
+                xDetected = true;
+                iCount = 0;
+            }
+            else if (tok == 'v' && xDetected == false) {
                 digit +=  5;
+                vDetected = true;
+                iCount = 0;
+                vCount += 1;
+            }
             else
                 throw "Illegal Argument Exception";
         }
+        if (iCount > 3 || vCount > 1)
+            throw "Illegal Argument Exception";
+            
     }
     return digit;
 }
